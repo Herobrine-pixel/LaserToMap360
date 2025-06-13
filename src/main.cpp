f983dc64e4b9f8551828fc509435b239795bbb6e
Herobrine Sah <ashasah77@gmail.com>
2025-06-13 14:05:33 +0530

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <LaserToMap.h>
+
+LaserToMap mapper;
+
+void setup() {
+  Serial.begin(115200);
+  mapper.begin();
+  delay(1000);
+}
+
+void loop() {
+  mapper.scan360();
+  mapper.printMap();
+  delay(3000);
+}
