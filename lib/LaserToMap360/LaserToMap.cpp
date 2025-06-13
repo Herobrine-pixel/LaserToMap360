eb43509948b1bd0d8d8017a37139a7ba8b708921
Herobrine Sah <ashasah77@gmail.com>
2025-06-13 14:28:06 +0530

    Add files via upload

---
+++ lib/LaserToMap360/LaserToMap.cpp
@@ -0,0 +1,35 @@
+#include "LaserToMap.h"
+
+LaserToMap::LaserToMap()
+  : stepper(AccelStepper::HALF4WIRE, 8, 10, 9, 11) {}
+
+void LaserToMap::begin() {
+  Wire.begin();
+  sensor.init();
+  sensor.setTimeout(500);
+  sensor.startContinuous();
+  stepper.setMaxSpeed(1000);
+  stepper.setAcceleration(500);
+  stepper.setSpeed(500);
+}
+
+void LaserToMap::scan360() {
+  int stepsPerDegree = LaserToMap::totalSteps / 360;
+
+  for (int angle = 0; angle < 360; angle++) {
+    stepper.runToNewPosition(angle * stepsPerDegree);
+    delay(20);
+    distances[angle] = sensor.readRangeContinuousMillimeters();
+  }
+
+  // Return to 0 position
+  stepper.runToNewPosition(0);
+}
+
+void LaserToMap::printMap() {
+  for (int angle = 0; angle < 360; angle++) {
+    Serial.print(angle);
+    Serial.print(",");
+    Serial.println(distances[angle]);
+  }
+}
